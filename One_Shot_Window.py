8cceb7a94f4a7addca0026d75d17f4b2948e9bd0
Saurav Mishra <saurav.mishra@live.com>
2020-11-10 12:42:33 +0530

    Create One_Shot_Window.py
    A simple login screen with button interactions.
    
    A window layout which consists of a username label, input text box, a password label and a password textbox where the input characters are masked.

---
+++ One_Shot_Window.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+
+# Define the window layout which consists of a username label,
+# input text box, a password label and a password textbox where the
+# input characters are masked.
+layout = [[sg.Text("Name:")],
+          [sg.Input(key='-INPUT-')],
+          [sg.Text('Password:')],
+          [sg.InputText('', key='Password', password_char='*')],
+          [sg.Button('OK'), sg.Button('Cancel')]]
+
+# Create the window
+window = sg.Window('Login', layout)
+
+# Display and interact with the Window using an Event Loop
+while True:
+    event, values = window.read()
+    # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED or event == 'Cancel':
+        break
+    
+    # if the usre clicks OK, display a message with login success. 
+    # No validation is added at this point as this is purely to 
+    # introduce the GUI concepts.
+    if event == 'OK':
+        sg.Popup('Login Result', 'SUCCESS!', modal = True)
+    
+# Finish up by removing from the screen
+window.close()
