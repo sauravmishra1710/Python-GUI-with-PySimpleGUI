49aa75c1c335d647617a654cdb466d1b309bb250
Saurav Mishra <saurav.mishra@live.com>
2020-11-10 12:13:00 +0530

    Create Hello_GUI.py
    A simple UI dialog.

---
+++ Hello_GUI.py
@@ -0,0 +1,22 @@
+import PySimpleGUI as sg
+
+# Define the window's contents
+layout = [[sg.Text("What's your name?")],
+          [sg.Input(key='-INPUT-', right_click_menu=['&Right', ['Display', 'E&xit']])],
+          [sg.Text(size=(40,1), key='-OUTPUT-')],
+          [sg.Button('Ok'), sg.Button('Quit')]]
+
+# Create the window
+window = sg.Window('Hello', layout)
+
+# Display and interact with the Window using an Event Loop
+while True:
+    event, values = window.read()
+    # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED or event == 'Quit' or event == 'Exit':
+        break
+    # Output a message to the window
+    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'])
+
+# Finish up by removing from the screen
+window.close()
